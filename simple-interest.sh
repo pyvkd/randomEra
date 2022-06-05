dd748aca2283560afb59cbcdcb7657e421f3ed1c
Vinay <vinay.not.nice@gmail.com>
2022-06-05 13:15:56 +0100

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,9 +4,9 @@
 
 # Do not use this in production. Sample purpose only.
 
-Author: Upkar Lidder (IBM)
-Additional Authors:
-pyvkd
+# Author: Upkar Lidder (IBM)
+# Additional Authors:
+# pyvkd
 
 # Input:
 # p, principal amount
